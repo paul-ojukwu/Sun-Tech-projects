8fe95aade263b2f925334e71a55de23d1c7da96d
paul-ojukwu <paulojukwu98@gmail.com>
2019-03-21 17:49:47 +0100

    the css, js and bootstrap projects have been added too

---
+++ CSS Project/app.js
@@ -0,0 +1,5 @@
+var init = function() {
+    document.querySelector('.jsOff').classList.remove('jsOff');
+    document.querySelector('.jsOff').classList.add('jsOn');
+    
+}
---
+++ Javascript project/app.js
@@ -0,0 +1,6 @@
+var jsCheck = function() {
+
+    document.querySelector('.Body').classList.remove('jsOff');
+    document.querySelector('.Body').classList.add('jsOn');
+    
+}
